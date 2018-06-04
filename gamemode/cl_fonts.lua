92cc6004c8477d17ffa2e99279ef5ca99a365a4e
Fexa <yns__gl@hotmail.com>
2018-06-04 22:41:01 +0300

    add rounds ui, delete unneccessary files

--- gamemode/cl_fonts.lua
+++ gamemode/cl_fonts.lua
@@ -1,5 +1,11 @@
-surface.CreateFont( "Arial24", {
+surface.CreateFont("Arial24", {
 	font = "Arial", 
 	size = 24,
 	weight = 500,
-} )
+})
+
+surface.CreateFont("Roboto24-300", {
+	font = "Roboto", 
+	size = 24,
+	weight = 300,
+})
