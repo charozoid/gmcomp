c3b1cbafc770ec01bc366bd1c8f69107157759c7
charozoid <directfromquebec@gmail.com>
2018-07-01 17:57:12 -0400

    Added partial name completion

--- gamemode/modules/chatcommands/sv_init.lua
+++ gamemode/modules/chatcommands/sv_init.lua
@@ -8,14 +8,25 @@ function GM:PlayerSay(sender, string)
 					local var = string.sub(string, argstart + 1)
 					if not tonumber(var) then
 						local plytbl = player.GetAll()
+						local foundtbl = {}
 						for o,p in pairs(plytbl) do
-							if string.lower(p:Nick()) == var then
+							local nick = string.lower(p:Nick())
+							if string.lower(nick) == var then
 								v.func(p)
 								break
 							end
-							if o==#plytbl then
+							if string.find(nick, var) then
+								table.insert(foundtbl, p)
+							end
+							if o==#plytbl and #foundtbl == 0 then
+								local fstart, fend, string = string.find()
 								print("No player found")
 							end
+						end
+						if #foundtbl > 1 then
+							print("Many players with this name")
+						else
+							v.func(foundtbl[1])
 						end	
 					else
 						local num = tonumber(var)
